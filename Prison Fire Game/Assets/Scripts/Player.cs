730301f2f7c1691cae81f4a7eda6139742c7ae63
CagedGenie295 <40520905+CagedGenie295@users.noreply.github.com>
2018-06-23 11:48:36 -0400

    Movement Script6/23/18 11:48
    Created a script for player movement, added barriers so player doesn't jump off screen

---
+++ Prison Fire Game/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float PlayerSpeed;
+	public Rigidbody2D RB;
+    public float JumpSpeed;
+    bool CanJump = true;
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetKeyDown("d"))
+        {
+            RB.AddForce(Vector2.right * Input.GetAxis("Horizontal") * PlayerSpeed);
+        }
+        if (Input.GetButtonDown("Jump") && CanJump == true)
+        {
+            RB.AddForce(Vector2.up * JumpSpeed);
+            CanJump = false;
+        }
+	}
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (RB.velocity.y == 0)
+        {
+            CanJump = true;
+        }
+    }
+
+}
