e55550f7cbb790aea7aade488ae035a079e6ddbb
CagedGenie295 <40520905+CagedGenie295@users.noreply.github.com>
2018-06-25 15:05:58 -0400

    Crowbar Edits 3:05 6/25/18
    Crowbar now Knocks Out enemies.

---
+++ Prison Fire Game/Assets/Crowbar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crowbar : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Police")
+        {
+            Destroy(col.gameObject);
+        }
+
+        if (col.gameObject.name == "Enemy NPC")
+        {
+            Destroy(col.gameObject);
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
