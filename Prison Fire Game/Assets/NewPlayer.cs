67df612e79970ba69ede868a11f9ae91975ad189
CagedGenie295 <40520905+CagedGenie295@users.noreply.github.com>
2018-06-28 10:25:49 -0400

    Player Animation 6/28/18 10:25
    Added moving animation to player, used new player sprite

---
+++ Prison Fire Game/Assets/NewPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayer : MonoBehaviour {
+    public Rigidbody2D RB;
+    public float PlayerSpeed;
+    public float JumpSpeed;
+    bool CanJump = true;
+    public Animator Anim;
+    // Use this for initialization
+    void Update () {
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            Anim.Play("Idle");
+        }
+        else
+        {
+            Anim.Play("Move");
+        }
+
+        RB.AddForce(Vector2.right * Input.GetAxis("Horizontal") * PlayerSpeed);
+        if (Input.GetButtonDown("Jump") && CanJump == true)
+        {
+            RB.AddForce(Vector2.up * JumpSpeed);
+            CanJump = false;
+        }
+    }
+	
+	void OnCollisionStay2D (Collision2D collision)
+    { 
+		if(RB.velocity.y == 0)
+        {
+            CanJump = true;
+        }
+
+	}
+}
